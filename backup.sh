e2f837fb7252f5c1bec27a9fe58ff12dfc5fb1ad
energetiq <energetiq@outlook.com>
2024-05-15 14:26:37 +0200

    ..

--- backup.sh
+++ backup.sh
@@ -1,27 +1,24 @@
 #!/usr/bin/env bash
 
-
-
 function nginx () {
     mkdir -p /tmp/backup/nginx
-    rsync -av /etc/nginx/ /tmp/backup/nginx/
+    cp -R /etc/nginx/ /tmp/backup/nginx/
 }
 
 function apache () {
     mkdir -p /tmp/backup/apache
-    rsync -av /etc/apache/ /tmp/backup/apache/
+    cp -R /etc/apache/ /tmp/backup/apache/
 }
 
 function dhcp () {
     mkdir -p /tmp/backup/dhcp
-    rsync -av /etc/dhcp/ /tmp/backup/dhcp/
+    cp -R /etc/dhcp/ /tmp/backup/dhcp/
 }
 
 function bind () {
     mkdir -p /tmp/backup/bind
-    rsync -av /etc/bind/ /tmp/backup/bind/
+    cp -R /etc/bind/ /tmp/backup/bind/
 }
-
 
 function backup () {
     mkdir /tmp/backup/
@@ -30,7 +27,7 @@ function backup () {
     read -rep "Apache ? [Y/N] : " apachec
     read -rep "DHCP ? [Y/N] : " dhcpc
     read -rep "Bind ? [Y/N] : " bindc
-    
+
     if [[ "$nginxc" =~ ^([yY][eE][sS]|[yY]|[oO][uU][iI]|[oO])$ ]]; then
         nginx
     fi
@@ -46,14 +43,7 @@ function backup () {
 
     tar -czvf $outputdir.tar.gz /tmp/backup/
     rm -r /tmp/backup/
-
 }
-
-
-
-
-
-
 
 echo "------------------------------------------"
 echo "##########################################"
@@ -61,12 +51,9 @@ echo "          Endorium Backup Script          "
 echo "##########################################"
 echo "------------------------------------------"
 
-
-
 read -rep "Output File : " outputdir
 echo $outputdir
 echo "##########################################"
 echo "          Backup Setup                    "
 echo "##########################################"
 backup
-
